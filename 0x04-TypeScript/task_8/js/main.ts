bd526bf473bb8190c31e8b8d0c8428f037845024
pdanyomiportfolio <anyomiprince27@gmail.com>
2025-09-17 20:19:21 +0000

    teacher interface

---
+++ 0x04-TypeScript/task_8/js/main.ts
@@ -0,0 +1,23 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // allows additional attributes
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
